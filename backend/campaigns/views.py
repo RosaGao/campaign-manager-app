9e2f36e60aff6a3f66fadd3168319dd280854c00
Rosa <ygao112@jhu.edu>
2023-06-22 21:08:13 +0800

    feat: created api views for GET and POST

--- backend/campaigns/views.py
+++ backend/campaigns/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
+from rest_framework import generics, response, status
+from .serializers import CampaignSerializer, SubscriberSerializer
+from .models import Campaign, Subscriber
 
-# Create your views here.
-from django.http import HttpResponse
+
+# GET - return all campaigns
+class CampaignListApiView(generics.ListAPIView):
+    # allow for data serialization/deserialization/validation
+    serializer_class = CampaignSerializer
+
+    # `auto` return 200 OK once query_set is populated
+    # `auto` include serialized data as response body using serializer_class
+    def get_queryset(self):
+        return Campaign.objects.all()
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# GET - return detail of a campaign
+class CampaignDetailApiView(generics.GenericAPIView):
+    serializer_class = CampaignSerializer
+
+    # can implement any custom request method in GenericAPIView
+    def get(self, request, slug):
+        target_campaign = Campaign.objects.filter(slug=slug).first()
+        if target_campaign:
+            return response.Response(
+                self.serializer_class(target_campaign).data, status=status.HTTP_200_OK
+            )
+        else:
+            return response.Response(
+                "Campaign does not exist!", status=status.HTTP_404_NOT_FOUND
+            )
+
+
+# POST - create a Subscriber to a specific campaign
+class SubscribeToACampaignApiView(generics.CreateAPIView):
+    # return 201 Created if success
+    # return the new Subscriber
+    serializer_class = SubscriberSerializer
+
+    def get_queryset(self):
+        return Subscriber.objects.all()
