3967d87fdf4ddd64ab315b1036b2f5949bc68fc5
Rosa <ygao112@jhu.edu>
2023-06-22 21:09:12 +0800

    feat: configured admin settings

--- backend/campaigns/admin.py
+++ backend/campaigns/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Campaign, Subscriber
 
-# Register your models here.
+
+class CampaignModelAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "created_at", "updated_at")
+    search_fields = ("title", "description")
+    list_per_page = 5
+
+
+class SubscriberModelAdmin(admin.ModelAdmin):
+    list_display = ("email", "campaign", "created_at")
+    search_fields = (
+        "email",
+        "campaign__title",
+    )
+    list_per_page = 5
+
+
+admin.site.register(Campaign, CampaignModelAdmin)
+admin.site.register(Subscriber, SubscriberModelAdmin)
