88a345cb1ed6f00b8aa44aeade7418ed5ca429b2
Rosa <ygao112@jhu.edu>
2023-06-24 09:43:36 +0800

    feat: added build-time data fetching

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,33 @@
-import { merriweather, nunito } from '../lib/fonts'
+import CampaignPage from '../components/CampaignPage';
+import { Campaign } from '../lib/types';
+import Head from 'next/head'
 
-export default function Home() {
+// fetch data at build time
+async function getCampaigns(): Promise<Campaign[]> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}campaigns`, { next: { revalidate: 10 } })
+    if (!response.ok) {
+      return []
+    }
+    return response.json()
+  } catch (err) {
+    return []
+
+  }
+}
+
+export default async function Home() {
+  let campaigns: Campaign[] = await getCampaigns();
+
   return (
-    <>
-      <h1 className='font-merriweather'>Whereas recognition of the inherent dignity</h1>
-      <h1 className='font-nunito'>Whereas recognition of the inherent dignity</h1>
-    </>
+    <div className='overflow-scroll text-slate-100 bg-zinc-900 h-container'>
+      <Head>
+        <title>Campaign Manager: | Home</title>
+        <meta name="description" content='Campaign management made simple!'></meta>
+      </Head>
+      <CampaignPage {...campaigns} />
+    </div >
   )
 }
+
+
