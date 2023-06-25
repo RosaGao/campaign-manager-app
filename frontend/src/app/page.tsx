08843877f2df039805bca4bcc54a1d2bfedc4d5c
Rosa <ygao112@jhu.edu>
2023-06-25 14:31:27 +0800

    fix: modified home page to look nicer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import CampaignPage from '../components/CampaignPage';
 import { Campaign } from '../lib/types';
 import Head from 'next/head'
+import Form from '@/components/Form';
 
 // fetch data at build time
-async function getCampaigns(): Promise<Campaign[]> {
+export async function getCampaigns(): Promise<Campaign[]> {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}campaigns`, { next: { revalidate: 10 } })
     if (!response.ok) {
@@ -20,13 +21,15 @@ export default async function Home() {
   let campaigns: Campaign[] = await getCampaigns();
 
   return (
-    <div className='overflow-scroll text-slate-100 bg-zinc-900 h-container'>
+    <>
       <Head>
         <title>Campaign Manager: | Home</title>
         <meta name="description" content='Campaign management made simple!'></meta>
       </Head>
-      <CampaignPage {...campaigns} />
-    </div >
+      <div className='overflow-scroll text-slate-100 bg-zinc-900 h-container'>
+        <CampaignPage {...campaigns} />
+      </div >
+    </>
   )
 }
 
